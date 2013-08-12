e98ebcdf9328ed08e27883185d2e9915969bd848
Jon Stroop <jpstroop@gmail.com>
2013-08-12 11:14:48 -0400

    validations

--- app/models/foaf.rb
+++ app/models/foaf.rb
@@ -1,2 +1,25 @@
+require 'uri'
+
 class Foaf < ActiveRecord::Base
+
+  validates :name, :work, :ident, :birthday, presence: true
+
+  # validates :ident, 
+
+  validates :work, allow_blank: true, format: {
+    with: URI::regexp,
+    message: 'must be a valid URI.'
+  }
+
+  validates :ident, uniqueness: true, format: {
+    with: /\A[[a-z]]{1,}\z/,
+    message: 'may only consist of lowercase letters and no spaces'
+  }
+
+  # Need a function that makes sure the value is a date. This doesn't work:
+  # validates :birthday, allow_blank: true, format: {
+  #   with: /\A\d{4}\z/,
+  #   message: 'must be formatted as YYYY-MM-DD.'
+  # }
+
 end
