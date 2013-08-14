752e8962364b8d92c0cd766eb374e1cc2e515f9e
Jon Stroop <jpstroop@gmail.com>
2013-08-14 17:19:10 -0400

    interests json only

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 LdrgFoaf::Application.routes.draw do
-  resources :interests
+  resources :interests, only: :index, defaults: { format: 'json' }
 
   resources :foafs
+
+  # resources :interests
+
+  # get 'interests' => 'interests#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
