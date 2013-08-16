94ee123149bffd9e9de87e944ce2df70488c3bd0
Jon Stroop <jpstroop@gmail.com>
2013-08-16 17:45:16 -0400

    first pass at #5. Closes #5 for now

--- app/helpers/foafs_helper.rb
+++ app/helpers/foafs_helper.rb
@@ -1,2 +1,9 @@
 module FoafsHelper
+  
+  def uri_from_foaf(f)
+    current_uri = RDF::URI.new("http://#{request.host}#{request.fullpath}")
+    path = Foaf.strip_extension_from_uri(current_uri)
+   "#{path}/#{f.slug}#me"
+  end
+
 end
